a44930f1bdf7f156b698396ec0972dfd5d656f07
ShahinCsa <shahin@amiti.in>
2019-05-14 13:51:04 +0530

    array of strings are added

--- GitProj/src/com/git/dummy/HelloGit.java
+++ GitProj/src/com/git/dummy/HelloGit.java
@@ -2,6 +2,11 @@ package com.git.dummy;
 
 public class HelloGit {
 public static void main(String[] args) {
+	String ar[]={"parrot","dog","monkey"};
+	for(int i=0;i<ar.length;i++)
+	{
+		System.out.println("hello" + ar[i]);
+	}
 	System.out.println("new line is added");
 	System.out.println("hello git");
 }
