c3c61d0d5ab012795c4ce45a5bcebf40f69f9ea1
ShahinCsa <shahin@amiti.in>
2019-05-14 23:06:42 +0530

    Fruits name in 2nd commit is added

--- GitProj/src/com/git/dummy/HelloGit.java
+++ GitProj/src/com/git/dummy/HelloGit.java
@@ -2,12 +2,13 @@ package com.git.dummy;
 
 public class HelloGit {
 public static void main(String[] args) {
-	String ar[]={"parrot","dog","monkey"};
+	String ar[]={"parrot","dog","monkey","mango","grapes"};
 	for(int i=0;i<ar.length;i++)
 	{
 		System.out.println("hello" + ar[i]);
 	}
 	System.out.println("new line is added");
 	System.out.println("hello git");
+	System.out.println("fruits name is added now");
 }
 }
